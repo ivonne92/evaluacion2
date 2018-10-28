d76785a6825418a3596e9e5baaf1a08fe9db1715
ivonne92 <43704226+ivonne92@users.noreply.github.com>
2018-10-28 00:46:49 -0600

    Add files via upload

---
+++ src/main/java/com/example/ivonne/evalu/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ivonne.evalu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+
+    }
+}
